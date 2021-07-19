71854ec3ffca93efc464c7eb661d39c744d9beda
mazenais <mazen.aboismail@gmail.com>
2021-07-19 16:48:45 +0200

    experimenting with modern javascript and ajax and fetch

--- main.js
+++ main.js
@@ -67,3 +67,67 @@ const arrowFunction=(msg) => {
     console.log(msg)
 }
 arrowFunction(`that's fucking awesome!`)
+
+
+//array functions
+//console.log("data", data)
+
+// data.forEach((data, i) => {
+//     console.log("i", i)
+//     console.log("data", data)
+// })
+
+// data.forEach(function (data) {
+//     console.log("data", data)
+// })
+
+// filter
+const filteredArray= data.filter((data, i) => {
+    return data.used === true
+})
+console.log(`filteredArray`, filteredArray)
+
+//map
+const mappedArray= data.map((data, i) => {
+    if (i === 3) {
+        data.text= "Busted"
+    }
+    return data
+})
+
+//spread operator
+const mappedArrayOne= data.map((data, i) => {
+    return {
+        ...data,
+        text: "What"
+    }
+})
+
+console.log(`mappedArray`, mappedArrayOne)
+console.log(`data`, data)
+
+//AJAX & Fetch
+// const url= "https://apis.is/earthquake/is?content-type=big bang"
+// let data
+// function getData() {
+//     fetch(url).then(response=> {
+//         console.log(`response`, response)
+//         return response.json()
+//     })
+//         .then(data=> {
+//             document.getElementById("loader").style.display="none"
+//             //console.log(`data`, data)
+//             data = data.results
+//             //console.log(`data`, data)
+//             renderTable()
+//     })
+// }
+//console.log(`data`, data)
+
+//templating 
+const mazen=34
+const anna = 27
+const greetingMessage= "hello" + mazen + "you are cooler than anna!"
+const templating = `hi ${mazen} how are you doing? `
+
+console.log(templating)

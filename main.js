8ad0d454e3110ee100e4dfe7a1c1706bdfdbaad4
mazenais <mazen.aboismail@gmail.com>
2021-07-21 02:19:48 +0200

    creating a table of data and adding social media links

--- main.js
+++ main.js
@@ -1,40 +1,39 @@
 console.log(data);
 
-function createTable() {
-    var tbody = document.getElementById("tbody")
-    // var button3 = document.createElement("button")
-    // button3.innerHTML = "that other button"
-    // button3.id = "button3"
-    // button3.className = "btn btn-primary"
-    for (var i = 0; i < data.length; i++) {
+// function createTable() {
+//     var tbody = document.getElementById("tbody")
+//     // var button3 = document.createElement("button")
+//     // button3.innerHTML = "that other button"
+//     // button3.id = "button3"
+//     // button3.className = "btn btn-primary"
+//     for (var i = 0; i < data.length; i++) {
 
+//         var tr = document.createElement("tr")
+//         if (i % 2 == 0) {
+//             tr.className = "table-primary"
+//         }
+//         else {
+//             tr.style.color = "blue"
+//         }
+//         var td1 = document.createElement("td")
+//         var td2 = document.createElement("td")
+//         var td3 = document.createElement("td")
 
-        var tr = document.createElement("tr")
-        if (i % 2 == 0) {
-            tr.className = "table-primary"
-        }
-        else {
-            tr.style.color = "blue"
-        }
-        var td1 = document.createElement("td")
-        var td2 = document.createElement("td")
-        var td3 = document.createElement("td")
+//         td1.innerHTML = data[i].name
+//         td2.innerHTML = data[i].summary
+//         td3.innerHTML = data[i].airdate
 
-        td1.innerHTML = data[i].name
-        td2.innerHTML = data[i].summary
-        td3.innerHTML = data[i].airdate
+//         tr.appendChild(td1)
+//         tr.appendChild(td2)
+//         tr.appendChild(td3)
+//         tbody.appendChild(tr)
 
-        tr.appendChild(td1)
-        tr.appendChild(td2)
-        tr.appendChild(td3)
-        tbody.appendChild(tr)
-
-    }
-}
-createTable()
+//     }
+// }
+// createTable()
 
 // function helloEvent() {
-   
+
 //     var button3 = document.getElementById("button3")
 //     button3.addEventListener("click", function (event) {
 //         console.log(`event`, event.target.id)
@@ -49,25 +48,23 @@ createTable()
 // })
 
 //hoisting function declaration
-console.log(functionBelow ("hello"))
-function functionBelow (greet) {
-    return `${greet} world`;
+console.log(functionBelow("hello"));
+function functionBelow(greet) {
+  return `${greet} world`;
 }
 console.log(functionBelow("Hi"));
 
-
 //function expression
-const myFunction=function (msg) {
-    console.log(msg)
-}
-myFunction("What's up?")
+const myFunction = function (msg) {
+  console.log(msg);
+};
+myFunction("What's up?");
 
 //function expression (arrow)
-const arrowFunction=(msg) => {
-    console.log(msg)
-}
-arrowFunction(`that's fucking awesome!`)
-
+const arrowFunction = (msg) => {
+  console.log(msg);
+};
+arrowFunction(`that's fucking awesome!`);
 
 //array functions
 //console.log("data", data)
@@ -82,29 +79,29 @@ arrowFunction(`that's fucking awesome!`)
 // })
 
 // filter
-const filteredArray= data.filter((data, i) => {
-    return data.used === true
-})
-console.log(`filteredArray`, filteredArray)
+const filteredArray = data.filter((e, _i) => {
+  return e.airdate.includes("2021-07-08")
+});
+console.log(`filteredArray`, filteredArray);
 
 //map
-const mappedArray= data.map((data, i) => {
-    if (i === 3) {
-        data.text= "Busted"
-    }
-    return data
-})
+const mappedArray = data.map((data, i) => {
+  if (i === 3) {
+    data.text = "Busted";
+  }
+  return data;
+});
 
 //spread operator
-const mappedArrayOne= data.map((data, i) => {
-    return {
-        ...data,
-        text: "What"
-    }
-})
+const mappedArrayOne = data.map((data, _i) => {
+  return {
+    ...data,
+    text: "What",
+  };
+});
 
-console.log(`mappedArray`, mappedArrayOne)
-console.log(`data`, data)
+console.log(`mappedArray`, mappedArrayOne);
+console.log(`data`, data);
 
 //AJAX & Fetch
 // const url= "https://apis.is/earthquake/is?content-type=big bang"
@@ -124,10 +121,96 @@ console.log(`data`, data)
 // }
 //console.log(`data`, data)
 
-//templating 
-const mazen=34
-const anna = 27
-const greetingMessage= "hello" + mazen + "you are cooler than anna!"
-const templating = `hi ${mazen} how are you doing? `
+//templating
+const mazen = 34;
+const anna = 27;
+const greetingMessage = "hello" + mazen + "you are cooler than anna!";
+const templating = `hi ${mazen} how are you doing? `;
 
-console.log(templating)
+console.log(templating);
+
+//fetching
+// fetch(" https://api.tvmaze.com/schedule/full")
+// .then((Response) => {
+//     console.log("we got a response but no body available yet");
+//     console.log(Response);
+//     return Response.json()
+// })
+// .then((jsonData) => {
+//     console.log("we got the data");
+//     console.log(jsonData);
+// })
+
+//create a function called 'searchShow(query)'
+function searchShow(query) {
+  const url = `https://api.tvmaze.com/search/shows?q=${query}`;
+  fetch(url)
+    .then(response => response.json())
+    .then((jsonData) => {
+      console.log(jsonData);
+
+    });
+}
+
+function createTable (data) {
+  
+    var tbody = document.getElementById("tbody")
+    var button3 = document.createElement("button")
+    button3.innerHTML = "that other button"
+    button3.id = "button3"
+    button3.className = "btn btn-primary"
+    
+    for (var i = 0; i < data.length; i++) {
+      
+      
+        var tr = document.createElement("tr")
+        if (i % 2 == 0) {
+            tr.className = "table-primary"
+        }
+        else {
+            tr.style.color = "blue"
+        }
+        var td1 = document.createElement("td")
+        var td2 = document.createElement("td")
+        var td3 = document.createElement("td")
+        var td4 = document.createElement("td")
+
+        td1.innerHTML = data[i].name
+        td2.innerHTML = data[i].summary
+        td3.innerHTML = data[i].airdate
+        td4.innerHTML = data[i].url
+
+        tr.appendChild(td1)
+        tr.appendChild(td2)
+        tr.appendChild(td3)
+        tr.appendChild(td4)
+        tbody.appendChild(tr)
+
+    }
+    tbody.appendChild(button3)
+}
+createTable()
+var button3 = document.getElementById("button3")
+    button3.addEventListener("click", function (event) {
+        console.log(`event`, event.target.id)
+    })
+    console.log("Hello button")
+
+
+var myButton = document.getElementById("myButton")
+
+myButton.addEventListener("click", function (event) {
+    console.log(`event`, event.target.id)
+})
+
+
+
+if (document.title== "Home") {
+    window.onload = () => {
+        const searchFieldElement = document.getElementById("searchField")
+        searchFieldElement.onkeyup = (event) => {
+            searchShow(searchFieldElement.value)
+        };
+    }
+
+}
